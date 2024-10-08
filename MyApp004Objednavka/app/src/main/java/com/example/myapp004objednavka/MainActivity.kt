06d22c0654d5613ea4b446e3333b83f6c5bf49eb
Petr Motl <petr.motl5@seznam.cz>
2024-10-08 18:07:55 +0200

    Nahradni branch

---
+++ MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.myapp004objednavka
+
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp004objednavka.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // enableEdgeToEdge()
+
+
+        // binding settings
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = "Objednávka kola"
+
+        binding.btnObjednavka.setOnClickListener {
+
+            // načtení ID vybraného radioButtonu z radioGroup
+            val koloRbId = binding.rgKola.checkedRadioButtonId
+
+            val kolo = findViewById<RadioButton>(koloRbId)
+
+            val vidlice = binding.cbVidlice.isChecked
+            val sedlo = binding.cbSedlo.isChecked
+            val riditka = binding.cbRiditka.isChecked
+
+            val objednavkaText = "Souhrn objednávky: " +
+                    "${kolo.text}" +
+                    (if(vidlice) "; lepší vidlice" else "") +
+                    (if(sedlo) "; lepší sedlo" else "") +
+                    (if(riditka) "; vytuněná karbonová řidítka" else "")
+
+            binding.tvObjednavka.text = objednavkaText
+
+        }
+
+        // Změna obrázku v závislosti na vybraném radioButtonu
+
+        binding.rbKolo1.setOnClickListener {
+            binding.ivKolo.setImageResource(R.drawable.img)
+        }
+
+        binding.rbKolo2.setOnClickListener {
+            binding.ivKolo.setImageResource(R.drawable.img_1)
+        }
+
+        binding.rbKolo3.setOnClickListener {
+            binding.ivKolo.setImageResource(R.drawable.img_2)
+        }
+
+    }
+}
